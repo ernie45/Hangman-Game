0ddfc13fa760c80fcc7af2bf2bcf71e0754e7614
Ernesto Hernandez <h_ernesto@ymail.com>
2018-02-05 01:18:40 -0800

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,43 +26,41 @@ if (gameIndex < words.length - 1){
 
 	/** Upon clicking a letter */
 	document.onkeyup = function(event){
-
-		/** Display the lines for each letter in the current word to guess */
-		document.querySelector("#correctGuess").innerHTML = currentWordArray.join(" ");
-		document.querySelector("#correctGuess").style.visibility="hidden";
-		/** Retreive the value of the key that was pressed */
-		/** Will retreive once the key is up again */
-		var letterChosen = event.key;
-		/** Parse the letter chosen to see if it's inside the word to guess */
-		parseLetter(letterChosen);
-		/** If the user guesses a correct letter */
-		if (isInWordToGuess){
-			/** Update the status of the correct letters chosen */
-			updateCurrentWordArray(letterChosen);
-		}
-		/** If the user guesses incorrectly */
-		else {
-			/** Update the status of the wrong letters */
-			updateWrongLetterArray(letterChosen);
-			/** Decrement the turns remaining */
-			turnsRemaining--;
-		}
-		/** Refresh the view to show the current status of the word to guess */
-		displayCurrentWord();
-		/** Show current game status */
-		updateGameStatus();
-		/** Check if the current game has been won or not */
-		checkGameCompletion();
-		/** If the current game IS complete */
-		if (isCurrentGameComplete){
-			/** Increment the gameNumber */
-			gameIndex++;
-			/** Create a new game with the next word */
-			setNewGame();
-			/** Make space for each letter in the new word */
-			createWordSpace();
-			/** Reset the complete boolean to false */
-			isCurrentGameComplete = false;
+		if (turnsRemaining > 0){
+			/** Display the lines for each letter in the current word to guess */
+			document.querySelector("#correctGuess").innerHTML = currentWordArray.join(" ");
+			document.querySelector("#correctGuess").style.visibility="hidden";
+			/** Retreive the value of the key that was pressed */
+			/** Will retreive once the key is up again */
+			var letterChosen = event.key;
+			/** Parse the letter chosen to see if it's inside the word to guess */
+			parseLetter(letterChosen);
+			/** If the user guesses a correct letter */
+			if (isInWordToGuess){
+				/** Update the status of the correct letters chosen */
+				updateCurrentWordArray(letterChosen);
+			}
+			/** If the user guesses incorrectly */
+			else {
+				/** Decrement the turns remaining */
+				if (currentWord.indexOf(letterChosen) === parseInt(-1)){
+					/** Refresh lives as well as status of word to guess */
+					updateWrongLetterArrayAndLives(letterChosen);
+				}
+			}
+			/** Refresh the view to show the current status of the word to guess */
+			displayCurrentWord();
+			/** Show current game status */
+			updateGameStatus();
+			/** Check if the current game has been won or not */
+			checkGameCompletion();
+			/** If the current game IS complete */
+			if (isCurrentGameComplete){
+				/** Increment games won */
+				wins++;
+				/** Create a new game with the next word */
+				setNewGame();
+			}
 		}
 	}	
 }
@@ -75,31 +73,40 @@ if (gameIndex < words.length - 1){
 
 /** If the letter is in the word to guess, append to correctLetter */
 function parseLetter(letterChosen){
-	/** Reset the boolean to false */
-	isInWordToGuess = false;
-	/** Traverse the current word's length */
-	for (let i = 0; i < currentWord.length; i++){
-		/** Check if the letter that was chosen is a letter in the current word */
-		if (letterChosen === currentWord.charAt(i)){
-			/** Change the boolean to true */
-			isInWordToGuess = true;
+	if (gameIndex < words.length - 1){
+		/** Reset the boolean to false */
+		isInWordToGuess = false;
+		/** Traverse the current word's length */
+		for (let i = 0; i < currentWord.length; i++){
+			/** Check if the letter that was chosen is a letter in the current word */
+			if (letterChosen === currentWord.charAt(i)){
+				/** Change the boolean to true */
+				isInWordToGuess = true;
+			}
 		}
 	}
 };
 /** Update the array that contains the correct letters */
 function updateCurrentWordArray(letterChosen){
-	for (let i = 0; i < currentWord.length; i++){
-		/** Check if the letter that was chosen is a letter in the current word */
-		if (letterChosen === currentWord.charAt(i)){
-			/** Change the boolean to true */
-			currentWordArray[i] = letterChosen;
+	if (gameIndex < words.length){
+		for (let i = 0; i < currentWord.length; i++){
+			/** Check if the letter that was chosen is a letter in the current word */
+			if (letterChosen === currentWord.charAt(i)){
+				/** Change the boolean to true */
+				currentWordArray[i] = letterChosen;
+			}
 		}
 	}
 };
 /** Update array that holds the letters that were wrongfully chosen */
-function updateWrongLetterArray(letterChosen){
-	/** Push the wrong letter into the wrongletter array */
-	wrongLetter.push(letterChosen);
+function updateWrongLetterArrayAndLives(letterChosen){
+	/** If the wrongLetters array doesn't contain the letter chosen */
+	if (wrongLetter.indexOf(letterChosen) === parseInt(-1) && letterChosen.length === 1 && letterChosen.match(/[a-z]/i)){
+		/** Decrement the turns remaining */
+		turnsRemaining--;
+		/** Push the wrong letter into the wrongletter array */
+		wrongLetter.push(letterChosen);
+	}
 };
 /** Display the status of the current word to guess */
 function displayCurrentWord(){
@@ -120,33 +127,23 @@ function updateGameStatus(){
 };
 /** Check if the entire word was guessed correctly */
 function checkGameCompletion(){
-	/** This array will help see if the word was completely guessed correctly */
-	var temp = [];
-	/** Traverse the current word */
-	for (var i = 0; i < currentWord.length; i++){
-		/** If the currentWordArray has the same letter as the currentWord */
-		if (currentWord[i] === currentWordArray[i]){
-			/** Push the correct letter onto the temp array */
-			temp.push(currentWord[i]);
+	if (gameIndex < words.length){
+		/** This array will help see if the word was completely guessed correctly */
+		var temp = [];
+		/** Traverse the current word */
+		for (var i = 0; i < currentWord.length; i++){
+			/** If the currentWordArray has the same letter as the currentWord */
+			if (currentWord[i] === currentWordArray[i]){
+				/** Push the correct letter onto the temp array */
+				temp.push(currentWord[i]);
+			}
+		}
+		/** If the temp array and currentWord have the same length */
+		if (temp.length === currentWord.length){
+			/** Then the word was guessed correctly */
+			isCurrentGameComplete = true;
 		}
 	}
-	/** If the temp array and currentWord have the same length */
-	if (temp.length === currentWord.length){
-		/** Then the word was guessed correctly */
-		isCurrentGameComplete = true;
-	}
-};
-/** Create a new game for the next level */
-function setNewGame(){
-	/** Reset the currentWordArray */
-	currentWordArray = [];
-	/** Reset the guesses remaining */
-	turnsRemaining = 12;
-	/** Reset the currentWord */
-	currentWord = words[gameIndex];
-	
-	/** Empty the wrong letter array */
-	wrongLetter = [];
 };
 /** Reserve a space for each letter in the word to guess */
 function createWordSpace(){
@@ -158,4 +155,37 @@ function createWordSpace(){
 			currentWordArray.push("_");
 		}
 	}
+};
+/** Create a new game for the next level */
+function setNewGame(){
+	/** Increment the gameNumber */
+	gameIndex++;
+	/** Reset the currentWordArray */
+	currentWordArray = [];
+	/** Reset the guesses remaining */
+	turnsRemaining = 12;
+	/** Reset the currentWord */
+	currentWord = words[gameIndex];
+	/** Empty the wrong letter array */
+	wrongLetter = [];
+	/** Make space for each letter in the new word */
+	createWordSpace();
+	/** Reset the complete boolean to false */
+	isCurrentGameComplete = false;
+};
+function resetGame(){
+	/** Increment the gameNumber */
+	gameIndex = 0;
+	/** Reset the currentWordArray */
+	currentWordArray = [];
+	/** Reset the guesses remaining */
+	turnsRemaining = 12;
+	/** Reset the currentWord */
+	currentWord = words[gameIndex];
+	/** Empty the wrong letter array */
+	wrongLetter = [];
+	/** Make space for each letter in the new word */
+	createWordSpace();
+	/** Reset the complete boolean to false */
+	isCurrentGameComplete = false;
 };
